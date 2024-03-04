54981287f19c01bf777731ca4631b878acd5eff1
Russ Garrett <russ@garrett.co.uk>
2024-03-04 21:59:30 +0000

    Don't give browsers two icons the same size with different purposes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -45,12 +45,6 @@ export default defineConfig({
                         type: 'image/png',
                     },
                     {
-                        src: 'pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any',
-                    },
-                    {
                         src: 'maskable-icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
