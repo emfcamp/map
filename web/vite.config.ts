8b1fd76e82010f508d0a9524cba6f587253dbb54
Russ Garrett <russ@garrett.co.uk>
2026-05-31 00:00:15 +0100

    Build grist widget

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,66 +1,71 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
 import { VitePWA } from 'vite-plugin-pwa'
 import { renderSVG } from 'vite-plugin-render-svg'
 
 export default defineConfig({
-    base: './',
-    build: {
-        // Suppress large chunk warning. There's not much we can do about this.
-        chunkSizeWarningLimit: 1000,
-        sourcemap: true,
-        rollupOptions: {
-            output: {
-                manualChunks: (id) => {
-                    if (id.includes('node_modules/maplibre-gl')) {
-                        return 'maplibre'
-                    }
-                },
-            },
+  base: './',
+  build: {
+    // Suppress large chunk warning. There's not much we can do about this.
+    chunkSizeWarningLimit: 1000,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: resolve(import.meta.dirname, 'index.html'),
+        'grist-widget': resolve(import.meta.dirname, 'grist-widget.html'),
+      },
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules/maplibre-gl')) {
+            return 'maplibre'
+          }
         },
+      },
     },
-    plugins: [
-        VitePWA({
-            devOptions: {
-                enabled: false,
-                type: 'module',
-            },
-            strategies: 'injectManifest',
-            srcDir: 'src',
-            filename: 'sw.ts',
-            registerType: 'autoUpdate',
-            includeAssets: ['fonts/**/0-255.pbf', 'sprites/**', 'icon.svg', 'favicon.ico', '*.png'],
-            manifest: {
-                id: '/',
-                short_name: 'EMF Map',
-                name: 'Electromagnetic Field 2024 Map',
-                description: 'Map for Electromagnetic Field 2024',
-                icons: [
-                    {
-                        src: 'pwa-64x64.png',
-                        sizes: '64x64',
-                        type: 'image/png',
-                    },
-                    {
-                        src: 'pwa-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                    },
-                    {
-                        src: 'maskable-icon-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable',
-                    },
-                ],
-                background_color: '#eeeeee',
-                display: 'standalone',
-                theme_color: '#FB48C4',
-            },
-        }),
-        renderSVG({
-            pattern: 'src/icons/*.svg',
-            urlPrefix: 'icons/',
-            outputOriginal: true,
-        }),
-    ],
+  },
+  plugins: [
+    VitePWA({
+      devOptions: {
+        enabled: false,
+        type: 'module',
+      },
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      includeAssets: ['fonts/**/0-255.pbf', 'sprites/**', 'icon.svg', 'favicon.ico', '*.png'],
+      manifest: {
+        id: '/',
+        short_name: 'EMF Map',
+        name: 'Electromagnetic Field 2024 Map',
+        description: 'Map for Electromagnetic Field 2024',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        background_color: '#eeeeee',
+        display: 'standalone',
+        theme_color: '#FB48C4',
+      },
+    }),
+    renderSVG({
+      pattern: 'src/icons/*.svg',
+      urlPrefix: 'icons/',
+      outputOriginal: true,
+    }),
+  ],
 })
