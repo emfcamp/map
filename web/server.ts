59272c2ae07de3248880e5c510912561fa311d93
Russ Garrett <russ@garrett.co.uk>
2026-07-06 01:04:06 +0100

    Set cache-control headers

--- web/server.ts
+++ web/server.ts
@@ -4,6 +4,22 @@ const PORT = 8080
 const HOST = '0.0.0.0'
 
 const app = express()
+
+const cached_paths = ['/assets', '/icons']
+
+cached_paths.forEach((dir) =>
+  app.use(
+    dir,
+    express.static(`./dist${dir}`, {
+      setHeaders: (res) => {
+        // Liberal CORS policy - we have no private data on this hostname
+        res.setHeader('Access-Control-Allow-Origin', '*')
+      },
+      maxAge: '90d',
+    })
+  )
+)
+
 app.use(
   express.static('./dist', {
     index: ['index.html'],
@@ -11,6 +27,7 @@ app.use(
       // Liberal CORS policy - we have no private data on this hostname
       res.setHeader('Access-Control-Allow-Origin', '*')
     },
+    maxAge: '5m',
   })
 )
 
